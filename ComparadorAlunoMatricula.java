8dfedbb52560ec369061f23453c2712520c9e7d6
KaioChisaki <147156827+KaioChisaki@users.noreply.github.com>
2025-09-14 20:42:43 -0300

    Delete ComparadorAlunoMatricula.java

--- ComparadorAlunoMatricula.java
+++
@@ -1,7 +0,0 @@
-import java.util.Comparator;
-
-public class ComparadorAlunoMatricula implements Comparator<Aluno> {
-    public int compare(Aluno a1, Aluno a2) {
-        return Integer.compare(a1.getMatricula(), a2.getMatricula());
-    }
-}
